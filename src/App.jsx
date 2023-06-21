848498652f2ceb21b5fb5c58a04f03027e3eac41
PedroHVMagalhaes <magalhaesphvm@gmail.com>
2023-06-21 10:30:30 -0300

    feat: componente da tabela (ToDoList) criado mas sem funcionalidades concluídas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.scss";
 import Header from "./Componentes/Header/Header";
 
-function App() {
+const App = ({ data }) => {
   return (
     <section className="body">
       <header>
@@ -10,20 +10,26 @@ function App() {
       <h1 className="title">
         Otimize seu tempo e se organize com o nosso Planejador Diário.
       </h1>
-      <div className="divTopic">
-        <div className="divTopic__tarefa">
-          <h2>Tarefa</h2>
-        </div>
-        <div className="divTopic__status">
-          <h2>Status</h2>
-        </div>
-        <div className="divTopic__opcoes">
-          <h2>Opções</h2>
-        </div>
-      </div>
-      <hr />
+      <table className="tftable">
+        <tr className="tftable__task">
+          <th>Tarefa</th>
+          <th>Status</th>
+          <th>Opções</th>
+        </tr>
+        {data.map((task) => (
+          <tr key={task.id} className="tftable__task">
+            <td>{task.title}</td>
+            <td>
+              <input type="checkbox" />
+            </td>
+            <td className="icones">
+              <img src="./editar.svg" height="20rem" />
+              <img src="./lixeira.svg" height="20rem" />
+            </td>
+          </tr>
+        ))}
+      </table>
     </section>
   );
-}
-
+};
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.scss";
+import { DB } from "./Mock/task.mock.js";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <App data={DB} />,
   },
 ]);
 
